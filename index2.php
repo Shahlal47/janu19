59337794bd1014c197f38c322e3572ca1cf7dc07
Shahlal47 <shahlalhossain@gmail.com>
2015-01-28 11:36:20 +0600

    make another static but interactive personal website

---
+++ index2.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ultimate Information System</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+</head>
+<body>
+<header class="container">
+    <section class="row">
+        <section class="col-md-12">
+            <a href="#"><img src="image/Ultimate.png"></a>
+            <ul class="nav nav-pills pull-right">
+                <br><br><br><br><br><br><br>
+                <li role="presentation" class="active"><a href="#">Home</a></li>
+                <li role="presentation"><a href="#">Profile</a></li>
+                <li role="presentation"><a href="#">Messages</a></li>
+                <li role="presentation"><a href="#">Messages</a></li>
+                <li role="presentation"><a href="#">Messages</a></li>
+            </ul>
+        </section>
+    </section>
+</header>
+<br>
+<section class="container">
+    <section class="row">
+        <section class="col-md-12">
+            <img src="image/blurry.jpg" class="img-responsive" alt="Responsive image" width="100%">
+        </section>
+    </section>
+</section>
+<br><br>
+<section class="container">
+    <section class="row">
+        <section class="col-md-4">
+            <h3>Creative Development</h3>
+            <p>This lorem ipsum generator is made for all the webdesigners, designers, webmasters and others who need lorem ipsum. Generator is made the way that everyone can use it, but especially for projects which need html markup. You can decide which html tags you want and our generator will generate just as you specified.</p>
+        </section>
+        <section class="col-md-4">
+            <h3>Apps Design & Development</h3>
+            <p>This lorem ipsum generator is made for all the webdesigners, designers, webmasters and others who need lorem ipsum. Generator is made the way that everyone can use it, but especially for projects which need html markup. You can decide which html tags you want and our generator will generate just as you specified.</p>
+        </section>
+        <section class="col-md-4">
+            <h3>Project Management</h3>
+            <p>This lorem ipsum generator is made for all the webdesigners, designers, webmasters and others who need lorem ipsum. Generator is made the way that everyone can use it, but especially for projects which need html markup. You can decide which html tags you want and our generator will generate just as you specified.</p>
+        </section>
+    </section>
+</section>
+<section class="container">
+    <section class="row">
+        <section class="col-md-12">
+            <div class="well well-sm"><h3>Intelligence Meets Imagination &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Call Us On +8801731479874 <button type="button" class="btn btn-primary btn-lg pull-right"> Request a Code</button></h3></div>
+        </section>
+    </section>
+</section>
+<section class="container">
+    <section class="row">
+        <section class="col-md-8">
+            <h3>About Our Company and Services</h3>
+            <p>
+                This lorem ipsum generator is made for all the webdesigners, designers, webmasters and others who need lorem ipsum. Generator is made the way that everyone can use it, but especially for projects which need html markup. You can decide which html tags you want and our generator will generate just as you specified.
+                This lorem ipsum generator is made for all the webdesigners, designers, webmasters and others who need lorem ipsum. Generator is made the way that everyone can use it, but especially for projects which need html markup. You can decide which html tags you want and our generator will generate just as you specified.
+            </p>
+        </section>
+        <section class="col-md-4">
+            <hr3>Contact Us</hr3>
+            <em><i>Address:</i></em>BCC Bhabon, Agargong, Dhaka<br>
+            <em><i>Telephone:</i></em>(+088) 02-98756546<br>
+            <em><i>Mobile:</i></em> +8801731479874<br>
+            <em><i>Email:</i></em> info.bcc@ictbd.com
+        </section>
+    </section>
+</section>
+<hr>
+<section class="container">
+    <section class="row">
+        <section class="col-md-4">
+            <img src="image/service%205.jpg" class="img-thumbnail" height="250" width="300">
+        </section>
+        <section class="col-md-4">
+            <img src="image/Service%204.jpg" class="img-thumbnail" height="250" width="300">
+        </section>
+        <section class="col-md-4">
+            <img src="image/services%203.jpg" class="img-thumbnail" height="250" width="300">
+        </section>
+    </section>
+</section>
+<hr>
+<footer class="container">
+    <section class="row">
+        <section class="col-md-6 pull-left">
+            &copy;Copyright 2015 @shahlal hossain
+        </section>
+        <section class="col-md-6 pull-right right">
+            <img src="image/1.png">
+            <img src="image/2.png">
+            <img src="image/3.png">
+            <img src="image/4.png">
+            <img src="image/5.png">
+            <img src="image/6.jpeg">
+        </section>
+    </section>
+</footer>
+<br><br><br>
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
